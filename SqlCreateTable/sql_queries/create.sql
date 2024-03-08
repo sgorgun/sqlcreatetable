15e81816b4560052a2fb33efa8de6d0e60e95436
sgorgun <sergei.gorgun@gmail.com>
2024-03-08 03:18:09 +0200

    Almost ready

--- SqlCreateTable/sql_queries/create.sql
+++ SqlCreateTable/sql_queries/create.sql
@@ -1,56 +1,55 @@
-CREATE TABLE [dbo].[emploee] (
-    [e_id]      INT           IDENTITY (1, 1) NOT NULL,
-    [e_name]    NVARCHAR (50) NULL,
-    [e_surname] NVARCHAR (50) NULL,
-    CONSTRAINT [PK_emploee] PRIMARY KEY CLUSTERED ([e_id] ASC)
+CREATE TABLE employee (
+    e_id INT IDENTITY (1, 1) NOT NULL,
+    e_name NVARCHAR (50) NULL,
+    e_surname NVARCHAR (50) NULL,
+    CONSTRAINT PK_employee PRIMARY KEY (e_id)
 );
 
-CREATE TABLE [dbo].[project] (
-    [p_id]                INT           IDENTITY (1, 1) NOT NULL,
-    [p_name]              NVARCHAR (50) NULL,
-    [p_date_of _creation] DATE          NULL,
-    [p_status]            CHAR (10)     NULL,
-    [p_data_of_closure]   DATE          NULL,
-    CONSTRAINT [PK_project] PRIMARY KEY CLUSTERED ([p_id] ASC)
+CREATE TABLE project (
+    p_id INT IDENTITY (1, 1) NOT NULL,
+    p_name NVARCHAR (50) NULL,
+    p_date_of_creation DATE NULL,
+    p_statuses CHAR (10) NULL,
+    p_date_of_closure DATE NULL,
+    CONSTRAINT PK_project PRIMARY KEY (p_id)
 );
 
-CREATE TABLE [dbo].[position] (
-    [pos_id]   INT        IDENTITY (1, 1) NOT NULL,
-    [pos_name] NCHAR (10) NULL,
-    CONSTRAINT [PK_position] PRIMARY KEY CLUSTERED ([pos_id] ASC)
+CREATE TABLE task (
+    t_id INT IDENTITY (1, 1) NOT NULL,
+    p_id INT NOT NULL,
+    t_description NVARCHAR (50) NULL,
+    t_deadline DATE NULL,
+    CONSTRAINT PK_task PRIMARY KEY (t_id),
+    CONSTRAINT FK_task_project_project FOREIGN KEY (p_id) REFERENCES project (p_id)
 );
 
-CREATE TABLE [dbo].[m2m_project_emploee] (
-    [e_id]   INT NOT NULL,
-    [p_id]   INT NOT NULL,
-    [pos_id] INT NOT NULL,
-    CONSTRAINT [FK_m2m_project_emploee_emploee] FOREIGN KEY ([e_id]) REFERENCES [dbo].[emploee] ([e_id]),
-    CONSTRAINT [FK_m2m_project_emploee_project] FOREIGN KEY ([p_id]) REFERENCES [dbo].[project] ([p_id]),
-    CONSTRAINT [FK_m2m_project_emploee_position] FOREIGN KEY ([pos_id]) REFERENCES [dbo].[position] ([pos_id])
+CREATE TABLE position (
+    pos_id INT IDENTITY (1, 1) NOT NULL,
+    pos_name NCHAR (10) NULL,
+    CONSTRAINT PK_position PRIMARY KEY (pos_id)
 );
 
-CREATE TABLE [dbo].[status] (
-    [s_id]   INT           IDENTITY (1, 1) NOT NULL,
-    [s_name] NVARCHAR (50) NULL,
-    CONSTRAINT [PK_status] PRIMARY KEY CLUSTERED ([s_id] ASC)
+CREATE TABLE statuses (
+    s_id INT IDENTITY (1, 1) NOT NULL,
+    s_name NVARCHAR (50) NULL,
+    CONSTRAINT PK_statuses PRIMARY KEY (s_id)
 );
 
-CREATE TABLE [dbo].[task] (
-    [t_id]          INT           IDENTITY (1, 1) NOT NULL,
-    [p_id]          INT           NOT NULL,
-    [t_description] NVARCHAR (50) NULL,
-    [t_deadline]    DATE          NULL,
-    CONSTRAINT [PK_task] PRIMARY KEY CLUSTERED ([t_id] ASC),
-    CONSTRAINT [FK_task_project] FOREIGN KEY ([p_id]) REFERENCES [dbo].[project] ([p_id])
+CREATE TABLE m2m_project_employee (
+    e_id INT NOT NULL,
+    p_id INT NOT NULL,
+    pos_id INT NOT NULL,
+    CONSTRAINT FK_m2m_project_employee_employee FOREIGN KEY (e_id) REFERENCES employee (e_id),
+    CONSTRAINT FK_m2m_project_employee_project FOREIGN KEY (p_id) REFERENCES project (p_id),
+    CONSTRAINT FK_m2m_project_employee_position FOREIGN KEY (pos_id) REFERENCES position (pos_id)
 );
 
-CREATE TABLE [dbo].[m2m_task_status] (
-    [s_id]   INT  NOT NULL,
-    [t_id]   INT  NOT NULL,
-    [s_date] DATE NULL,
-    [e_id]   INT  NULL,
-    CONSTRAINT [FK_m2m_task_status_emploee] FOREIGN KEY ([e_id]) REFERENCES [dbo].[emploee] ([e_id]),
-    CONSTRAINT [FK_m2m_task_status_task1] FOREIGN KEY ([t_id]) REFERENCES [dbo].[task] ([t_id]),
-    CONSTRAINT [FK_m2m_task_status_status1] FOREIGN KEY ([s_id]) REFERENCES [dbo].[status] ([s_id])
+CREATE TABLE m2m_task_statuses (
+    s_id INT NOT NULL,
+    t_id INT NOT NULL,
+    s_date DATE NULL,
+    e_id INT NULL,
+    CONSTRAINT FK_m2m_task_statuses_employee FOREIGN KEY (e_id) REFERENCES employee (e_id),
+    CONSTRAINT FK_m2m_task_statuses_task FOREIGN KEY (t_id) REFERENCES task (t_id),
+    CONSTRAINT FK_m2m_task_statuses_statuses FOREIGN KEY (s_id) REFERENCES statuses (s_id)
 );
-
